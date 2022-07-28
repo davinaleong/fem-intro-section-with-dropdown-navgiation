0c881bf22ef35967f3e4838349a136a5ac602033
Davina Leong <davina.leong.sy@gmail.com>
2022-07-28 16:14:25 +0800

    Done with mobile menu

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,21 @@
+const primaryHeader = document.querySelector(".primary-header");
+const primaryToggle = document.querySelector(".primary-header .toggle");
+
 const heroSection = document.querySelector(".hero");
 const dropdownToggles = document.querySelectorAll("a[data-toggle='dropdown']");
 const dropdownMenus = document.querySelectorAll(".dropdown-menu");
 
+primaryToggle.addEventListener("click", () => {
+  primaryHeader.toggleAttribute("data-open");
+});
+
 dropdownToggles.forEach((node) => {
   node.addEventListener("click", (e) => {
+    e.preventDefault();
+
     console.log("Dropdown clicked");
-    e.target.parentNode.childNodes[3].setAttribute("data-open", true);
+    e.target.toggleAttribute("data-open");
+    e.target.parentNode.childNodes[3].toggleAttribute("data-open");
   });
 });
 
